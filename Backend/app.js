4ad99b79d9ec69428ee5b1c5251de3bef43036cd
Hasnain <devxpert404@gmail.com>
2025-03-25 16:15:02 +0530

    Change in cros url with frontend real url

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 
 // CORS Configuration
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://todo-app-ljwp.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
