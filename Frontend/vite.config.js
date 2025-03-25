d7acca3ef691903f4d144104296cf4ab36aa84e8
Hasnain <devxpert404@gmail.com>
2025-03-25 15:43:53 +0530

    remove path code from backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -11,7 +10,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false
       },
-      "/todo": {  // Add this to proxy todo-related requests
+      "/todo": {
         target: "https://todo-app-7i4k.onrender.com",
         changeOrigin: true,
         secure: false
